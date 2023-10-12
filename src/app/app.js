d2f8e2ee4596aa0862d862917d1a11438670a393
nonsensetwice <joshua@nonsensecodes.io>
2023-10-11 18:52:45 -0700

    added lint script and fixed linting errors

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,11 @@
 import { readdir } from 'fs/promises';
 import { join } from 'path';
 import {
-    GatewayIntentBits,
-    Partials
-}
-from 'discord.js';
+	GatewayIntentBits,
+	Partials
+} from 'discord.js';
 import { SapphireClient } from '@sapphire/framework';
-import Log from './utils/Log.js'
+import Log from './utils/Log.js';
 import { init, Integrations } from '@sentry/node';
 import { RewriteFrames } from '@sentry/integrations';
 
@@ -18,69 +17,69 @@ initializeEvents();
 client.login(process.env.DISCORD_BOT_TOKEN).catch(Log.error);
 
 function initializeClient() {
-    const clientOptions = {
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMembers,
-            GatewayIntentBits.GuildEmojisAndStickers,
-            GatewayIntentBits.GuildVoiceStates,
-            GatewayIntentBits.GuildPresences,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.GuildMessageReactions,
-            GatewayIntentBits.GuildModeration,
-            GatewayIntentBits.DirectMessages,
-            GatewayIntentBits.GuildMessageReactions,
-        ],
-        partials: [
-            Partials.Message,
-            Partials.Channel,
-            Partials.Reaction,
-            Partials.User
-        ],
-    }
-    return new SapphireClient(clientOptions);
-};
+	const clientOptions = {
+		intents: [
+			GatewayIntentBits.Guilds,
+			GatewayIntentBits.GuildMembers,
+			GatewayIntentBits.GuildEmojisAndStickers,
+			GatewayIntentBits.GuildVoiceStates,
+			GatewayIntentBits.GuildPresences,
+			GatewayIntentBits.GuildMessages,
+			GatewayIntentBits.GuildMessageReactions,
+			GatewayIntentBits.GuildModeration,
+			GatewayIntentBits.DirectMessages,
+			GatewayIntentBits.GuildMessageReactions,
+		],
+		partials: [
+			Partials.Message,
+			Partials.Channel,
+			Partials.Reaction,
+			Partials.User
+		],
+	};
+	return new SapphireClient(clientOptions);
+}
 
 // the events handler
 async function initializeEvents() {
-    
-    client.on('error', Log.error);
+	
+	client.on('error', Log.error);
 
-    const eventsPath = new URL('./events', import.meta.url);
-    const eventsArray = await readdir(eventsPath);
-    const eventFiles = eventsArray.filter(file => file.endsWith('.js'));
-    // client.events = new Collection();
-    eventFiles.forEach(async function(file) {
-        const filePath = join(eventsPath.toString(), file);
-        const eventModule = await import(filePath);
-        const event = await eventModule.default;
-        // client.events.set(event.name, event);
-        try {
-            if (event.once) {
-              client.once(event.name, (...args) => event.execute(...args, client));
-            } else {
-              client.on(event.name, (...args) => event.execute(...args, client));
-            }
-          } catch (e) {
-            console.error('events error:', e);
-          }
-    })
-    return client.events;
-};
+	const eventsPath = new URL('./events', import.meta.url);
+	const eventsArray = await readdir(eventsPath);
+	const eventFiles = eventsArray.filter(file => file.endsWith('.js'));
+	// client.events = new Collection();
+	eventFiles.forEach(async function(file) {
+		const filePath = join(eventsPath.toString(), file);
+		const eventModule = await import(filePath);
+		const event = await eventModule.default;
+		// client.events.set(event.name, event);
+		try {
+			if (event.once) {
+				client.once(event.name, (...args) => event.execute(...args, client));
+			} else {
+				client.on(event.name, (...args) => event.execute(...args, client));
+			}
+		} catch (e) {
+			console.error('events error:', e);
+		}
+	});
+	return client.events;
+}
 
 // Sentry setup
 function initializeSentryIO() {
-    const root = new URL('./', import.meta.url);
-    init({
-        dsn: process.env.SENTRY_IO_DSN,
-        tracesSampleRate: 1.0,
-        environment: process.env.NODE_ENV,
-        
-        integrations: [
+	const root = new URL('./', import.meta.url);
+	init({
+		dsn: process.env.SENTRY_IO_DSN,
+		tracesSampleRate: 1.0,
+		environment: process.env.NODE_ENV,
+		
+		integrations: [
 			new RewriteFrames({
 				root: root,
 			}),
 			new Integrations.Http({ tracing: true }),
 		],
-    });
+	});
 }
--- src/app/utils/Log.js
+++ src/app/utils/Log.js
@@ -3,28 +3,28 @@ import Sentry from '@sentry/node';
 
 let pinoLevel;
 if (process.env.NODE_ENV != 'production') {
-    pinoLevel = 'trace';
+	pinoLevel = 'trace';
 } else {
-    pinoLevel = 'info';
+	pinoLevel = 'info';
 }
 
 const transport = pino.transport({
-    target: 'pino-sentry-transport',
-    options: {
-        name: process.env.APP_NAME,
-        level: pinoLevel,
-        batching: true,
-        interval: 5,
-        sentry: {
-            dsn: process.env.SENTRY_IO_DSN
-        }
-    }
+	target: 'pino-sentry-transport',
+	options: {
+		name: process.env.APP_NAME,
+		level: pinoLevel,
+		batching: true,
+		interval: 5,
+		sentry: {
+			dsn: process.env.SENTRY_IO_DSN
+		}
+	}
 });
 
 let logger;
 
 try {
-    logger = pino(transport);
+	logger = pino(transport);
 	logger.info('Logger initialized!');
 } catch (e) {
 	// eslint-disable-next-line no-console
@@ -32,95 +32,95 @@ try {
 	// eslint-disable-next-line no-console
 	console.log(e);
 	throw new Error();
-};
+}
 
 const Log = {
 
-    fatal(statement, options) {
-        logger.fatal(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'fatal',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	fatal(statement, options) {
+		logger.fatal(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'fatal',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    error(statement, options) {
-        logger.error(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'error',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	error(statement, options) {
+		logger.error(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'error',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    warn(statement, options) {
-        logger.warn(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'warn',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	warn(statement, options) {
+		logger.warn(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'warn',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    info(statement, options) {
-        logger.info(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'info',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	info(statement, options) {
+		logger.info(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'info',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    debug(statement, options) {
-        logger.debug(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'debug',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	debug(statement, options) {
+		logger.debug(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'debug',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    trace(statement, options) {
-        logger.trace(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'trace',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	trace(statement, options) {
+		logger.trace(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'trace',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    log(statement, options) {
-        logger.log(statement, options);
-        Sentry.addBreadcrumb({
-            level: 'log',
-            message: statement
-        });
-        if (process.env.NODE_ENV != 'production') {
-            // eslint-disable-next-line no-console
+	log(statement, options) {
+		logger.log(statement, options);
+		Sentry.addBreadcrumb({
+			level: 'log',
+			message: statement
+		});
+		if (process.env.NODE_ENV != 'production') {
+			// eslint-disable-next-line no-console
 			console.error(statement);
-        }
-    },
+		}
+	},
 
-    flush() { logger.flush(); }
+	flush() { logger.flush(); }
 
 };
 
